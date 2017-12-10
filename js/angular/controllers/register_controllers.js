694012f925e996699a92730385b425bb18cf2d3a
brian <franzbrianbriones@gmail.com>
2017-12-11 05:02:10 +0800

    login implementation

--- js/angular/controllers/register_controllers.js
+++ js/angular/controllers/register_controllers.js
@@ -6,5 +6,35 @@ angular.module('reme.controllers')
 registerController.$inject = ['$scope', 'registerApiService'];
 
 function registerController($scope, registerApiService) {
-	
+	var self = this;
+		self.errors = {};
+		self.reg = {};
+
+	self.submitRegister = function() {
+		self.errors = {};
+		$scope.requiredValidator($scope.reg.firstname, 'firstname');
+		$scope.requiredValidator($scope.reg.lastname, 'lastname');
+		$scope.validateEmail($scope.reg.email);
+		$scope.validatePassword($scope.reg.password);
+		$scope.checkGender($scope.reg.gender);
+
+		var err = $.map($scope.errors, function(e) {
+			if(e != false) {
+				return e;
+			}
+		});
+
+		if(err.length == 0) {
+			apiService.register($scope.reg).then(function(res) {
+				console.log(res);
+				if(res.data.data) {
+					$scope.landing = 'success_reg';
+				} else if(res.data.errors) {
+					$scope.errors = res.data.errors;
+				}
+			}).catch(function(res){
+				console.log(res.error);
+			})
+		}
+	}
 }
