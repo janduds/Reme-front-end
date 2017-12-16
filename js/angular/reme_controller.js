74bcea87603812648258485e5148578515516590
jsuizo <jayson.b.suizo@gmail.com>
2017-12-16 22:54:09 +0800

    update

--- js/angular/reme_controller.js
+++ js/angular/reme_controller.js
@@ -14,7 +14,7 @@ function remeController($scope, apiService) {
 		$scope.days =['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
 		$scope.months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 		//,
-		$scope.genders = ['male','female','other'];
+		$scope.genders = ['male','female','others'];
 
 
 		//initialize user
